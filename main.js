f2a59f38b25baeb148a3088a16f40a208668d810
Dushani-Ekanayake <kaviiekanayake2@gmail.com>
2025-07-19 15:46:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var typed = new Typed(".text", {
+  strings: [ "Technical Writer", "Lifelong Learner","Frontend Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
+var typed = new Typed(".text1", {
+  strings: [ "About Me"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+var typed = new Typed(".text3", {
+  strings: [ "My Skills"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+var typed = new Typed(".text4", {
+  strings: ["Contact Me"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
